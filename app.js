75d06b7840203cf8e4bdc92cc3104f748e3fd005
Vraja Das <vraja.pro@gmail.com>
2022-10-01 17:26:44 -0400

    starter

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const total = 10
+const express = require('express');
+const app = express();
+
+// routes
+app.get('/hello', (req,res)=>{
+    res.send('Hi!')
+})
+
+// get all the tasks
+app.get('/api/v1/tasks')
+
+const port = 3000
+
+app.listen(port, console.log(`Server listening on port ${port}`))
